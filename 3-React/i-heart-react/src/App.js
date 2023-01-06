4f219edffacf3353e34f68bc6bf46526d5db0413
anmil-dev <anmil.dev@gmail.com>
2023-01-06 15:14:01 -0500

    The App Component

---
+++ 3-React/i-heart-react/src/App.js
@@ -0,0 +1,35 @@
+import HeartsList from './components/HeartsList';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <Header />;
+  <HeartsList />;
+  </>
+  )
+};
+
+export default App;
+
+
+
+// (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>
+//         Edit <code>src/App.js</code> and save to reload.
+//       </p>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn React
+//       </a>
+//     </header>
+//   </div>
+// );
